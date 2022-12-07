ec0063305a44d954e62ae3a531224be95fa18bda
victoriazal <victoriazalomnova19@gmail.com>
2022-12-07 19:58:42 +0300

    feat: add further functionality, get rid of personal text

--- index.js
+++ index.js
@@ -9,25 +9,47 @@ function hideAllSlides(){
     }
 }
 
-document.getElementById("carousel-button-next").addEventListener('click', function moveToTheNextSlide() {
-    hideAllSlides()
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-        console.log(slidePosition)
-    }
-    slides[slidePosition].classList.add("carousel-item-visible")
-})
-document.getElementById("carousel-button-prev").addEventListener('click', function moveToThePrevSlide() {
-    hideAllSlides()
+// document.getElementById("carousel-button-next").addEventListener('click', function moveToTheNextSlide() {
+//     hideAllSlides()
+//     if (slidePosition === totalSlides - 1) {
+//         slidePosition = 0;
+//     } else {
+//         slidePosition++;
+//         console.log(slidePosition)
+//     }
+//     slides[slidePosition].classList.add("carousel-item-visible")
+// })
+// document.getElementById("carousel-button-prev").addEventListener('click', function moveToThePrevSlide() {
+//     hideAllSlides()
 
-    if (slidePosition === 0) {
-        slidePosition = totalSlides-1;
-    } else {
-        slidePosition--
-        console.log(slidePosition)
-    }
-    slides[slidePosition].classList.add("carousel-item-visible")
-})
+//     if (slidePosition === 0) {
+//         slidePosition = totalSlides-1;
+//     } else {
+//         slidePosition--
+//         console.log(slidePosition)
+//     }
+//     slides[slidePosition].classList.add("carousel-item-visible")
+// })
+
+
+// create a safe space after breathing exercise
+let container = document.getElementById("container")
+let moveOnBtn = document.getElementById("moveOnBtn")
+    moveOnBtn.addEventListener('click', function(){
+        container.innerHTML = `
+        <p class="supportive-text">
+        You are now being the strongest you have ver been, even if it doesn't feel like it
+        and I am really proud of you
+      </p>
+        `
+        moveOnBtn.innerHTML = 'I am strong'
+        if( moveOnBtn.innerHTML = 'I am strong'){
+            moveOnBtn.addEventListener("click", function(){
+                container.innerHTML = `
+                <p class="supportive-text">
+                YES YOU ARE!
+              </p>
+               `})
+        }
+    })
 
